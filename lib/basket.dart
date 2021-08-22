aa295a4ca7e350e9428d296a2774a9ded2189f08
Alili88 <w1111160212@gmail.com>
2021-08-22 17:57:51 +0800

    refactor: add discount mapping and remove hardcode return value

--- lib/basket.dart
+++ lib/basket.dart
@@ -1,17 +1,33 @@
 class Basket {
   List<int> _books = [];
+  Map<int, double> discountMapping = {
+    1: 1.0,
+    2: 0.95,
+    3: 0.9,
+    4: 0.8,
+    5: 0.75,
+  };
 
   void addBooks(List<int> books) {
     _books = books;
   }
 
   double getPrice() {
-    if (_books.where((element) => element == 1).length == 1) {
-      return 8;
+    double totalPrice = 0;
+    while (getTypes() > 0) {
+      var discount = discountMapping[getTypes()]!;
+      totalPrice += 8 * getTypes() * discount;
+      _books = _books.map((book) {
+        if (book > 0) {
+          book--;
+        }
+        return book;
+      }).toList();
     }
-    if (_books.where((element) => element == 1).length == 2) {
-      return 15.2;
-    }
-    return 0;
+    return totalPrice;
+  }
+
+  int getTypes() {
+    return _books.where((element) => element > 0).length;
   }
 }
